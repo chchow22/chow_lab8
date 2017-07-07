60518e4257e906b9dc9c5d8cbb8d0015302c8ca6
Chris <chchow@umich.edu>
2017-07-07 16:57:08 -0400

    second commit

--- shoppingList.js
+++ shoppingList.js
@@ -6,22 +6,8 @@ shampoo = {name: "Shampoo", price: 7};
 
 groceryArray = [toiletPaper, frozenYogurt, detergent, salmon, shampoo];
 
-var subTotal = 0;
-var taxTotal = 0;
-groceryArray.forEach(function(item) {
-	console.log("Item name: " + item.name);
-	console.log("Item price ($): " + item.price);
-	subTotal += item.price;
-	taxTotal += item.price * 0.06;
-});
 
 
-console.log("");
-console.log("Tax: $" + taxTotal);
-console.log("Subtotal: $" + subTotal);
-
-var total = subTotal + taxTotal;
-console.log("Total price: $" + total);
 
 
 
@@ -32,3 +18,48 @@ function makeArray(start, end) {
 	}
 	return longArray;
 }
+
+initList();
+function initList() {
+	var groceryList = document.getElementById("grocery");
+	for (var i = 0; i < groceryArray.length; i++) {
+		var listItem = document.createElement("li");
+		listItem.innerText = groceryArray[i].name + " costs $" + groceryArray[i].price;
+		groceryList.appendChild(listItem);
+	}
+	updateTotal();
+}
+
+function addToList() {
+	var itemName = document.getElementById("newGrocery").value;
+	var priceNum = document.getElementById("newPrice").value;
+	groceryArray.push({name: itemName, price: priceNum});
+	var groceryList = document.getElementById("grocery");
+	var listItem = document.createElement("li");
+	listItem.innerText = itemName + " costs $" + priceNum;
+	groceryList.appendChild(listItem);
+	updateTotal();
+}
+
+function updateTotal() {
+	var subTotal = 0;
+	var taxTotal = 0;
+	groceryArray.forEach(function(item) {
+		console.log("Item name: " + item.name);
+		console.log("Item price ($): " + item.price);
+		subTotal += item.price * 1;
+		taxTotal += item.price * 0.06;
+	});
+
+	console.log("");
+	console.log("Tax: $" + taxTotal);
+	console.log("Subtotal: $" + subTotal);
+
+	var total = subTotal + taxTotal;
+	console.log("Total price: $" + total);
+
+	var totalDisplay = document.getElementById("total");
+
+	totalDisplay.innerText = "Subtotal: $" + subTotal.toFixed(2) + "  Tax: $" + taxTotal.toFixed(2) + "  Total price: $" + total.toFixed(2);
+}
+
